06d7088c7215c1c37bc7d8e6d29e349d549f7f8c
NovikovNick <nicknovikov10@gmail.com>
2020-05-05 01:02:51 +0300

    use mouse position in player's movement in showcase

--- client/src/main/java/com/metalheart/service/PlayerInputService.java
+++ client/src/main/java/com/metalheart/service/PlayerInputService.java
@@ -1,11 +1,13 @@
 package com.metalheart.service;
 
+import com.metalheart.model.Force;
 import com.metalheart.model.Vector3;
 import com.metalheart.model.physic.Point2d;
-import com.metalheart.model.Force;
+import com.metalheart.repository.PlayerRepository;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
@@ -13,12 +15,15 @@ import java.awt.*;
 @Component
 public class PlayerInputService {
 
-    private static final int SPEED = 15;
+    private static final int SPEED = 6;
 
     private boolean wPressed;
     private boolean aPressed;
     private boolean sPressed;
     private boolean dPressed;
+
+    @Autowired
+    private PlayerRepository playerRepository;
 
     @Getter
     private EventHandler<KeyEvent> keyPressHandler = e -> {
@@ -69,11 +74,27 @@ public class PlayerInputService {
             return new Force(direction, 0);
         }
 
-        if (wPressed) direction.setY(direction.getY() + 1);
-        if (sPressed) direction.setY(direction.getY() - 1);
+        Point2d m = getMousePosition();
+        Point2d center = CanvasService.toLocalCoord(PhysicUtil.getCenter(playerRepository.get().getData()));
 
-        if (aPressed) direction.setX(direction.getX() - 1);
-        if (dPressed) direction.setX(direction.getX() + 1);
+        Vector3 normalized = new Vector3(m.getX() - center.getX(), -(m.getY() - center.getY()), 0).normalize();
+
+        if (wPressed) direction = normalized;
+
+        if (sPressed) direction = direction.plus(normalized.scale(-1));
+
+        if (aPressed) {
+            Point2d left = PhysicUtil.rotate(new Point2d(normalized.getX(), normalized.getY()), (float) Math.toRadians(90));
+            direction = direction.plus(new Vector3(left.getX(), left.getY(), 0));
+        }
+
+        if (dPressed){
+            Point2d right = PhysicUtil.rotate(new Point2d(normalized.getX(), normalized.getY()), (float) Math.toRadians(-90));
+            direction = direction.plus(new Vector3(right.getX(), right.getY(), 0));
+        }
+
+
+        System.out.println(direction);
 
         return new Force(direction, SPEED);
     }
--- core/src/main/java/com/metalheart/model/Vector3.java
+++ core/src/main/java/com/metalheart/model/Vector3.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import static java.lang.Math.sqrt;
+
 /**
  * todo: should be final
  */
@@ -17,4 +19,25 @@ public class Vector3 {
     private float x;
     private float y;
     private float z;
+
+    public Vector3 normalize() {
+        return scale(1 / magnitude());
+    }
+
+    public Vector3 plus(Vector3 v) {
+        return new Vector3(x + v.getX(), y + v.getY(), z + v.getZ());
+    }
+
+    public Vector3 scale(float s) {
+        return new Vector3(x * s, y * s, z * s);
+    }
+
+    public float magnitude() {
+        return (float) sqrt(x * x + y * y + z * z);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("v3(%.3f, %.3f, %.3f)", x, y, z);
+    }
 }
